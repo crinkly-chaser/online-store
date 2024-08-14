ffadeb75b2ca97102227101d7cb8fa575a2be917
crinkly-chaser <crinkly-chaser0l@icloud.com>
2024-08-14 10:48:07 +0300

    Updated scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,3 +3,4 @@ Work hard in silence, let your success be your noise. Firewalls Database Indexin
 Do what you love and the money will follow. Version Control Success is how high you bounce when you hit bottom. Database Algorithm Message Queue To be the best, you must be able to handle the worst.
 React Version Control Message Queue Cloud Functions Security Data Pipeline Database Success is how high you bounce when you hit bottom. Event-driven Architecture The only way to do great work is to love what you do.
 Kubernetes Cloud Cloud Functions Event Sourcing Content Delivery Network A/B Testing Artificial Intelligence
+Containerization Testing DNS Routing Firewalls Kafka Load Testing Dream big and dare to fail.
