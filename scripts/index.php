6cdd831c5f9ff281d1d9c3fc9deea870805369eb
crinkly-chaser <crinkly-chaser0l@icloud.com>
2024-11-14 10:47:57 +0300

    Updated scripts

--- scripts/index.php
+++ scripts/index.php
@@ -1,1 +1,2 @@
 Machine Learning A/B Testing React CI/CD JavaScript Data Science Scalability
+RabbitMQ Monitoring Message Queue Agile Data Pipeline Success is not in what you have, but who you are. Python Don't be afraid to give up the good to go for the great. The only way to do great work is to love what you do. IoT
